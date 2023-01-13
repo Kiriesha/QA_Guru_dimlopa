9db0f52cf6e390801760123a3224dabd7105f0e4
Kiriesha <49344266+Kiriesha@users.noreply.github.com>
2023-01-13 15:07:46 +0300

    Add attachments

--- src/test/Java/tests/ui/TestBase.java
+++ src/test/Java/tests/ui/TestBase.java
@@ -1,9 +1,14 @@
 package tests.ui;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import config.TestsConfig;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.AutorisationPage;
 import pages.MainPage;
 
@@ -13,7 +18,9 @@ public class TestBase {
 
     @BeforeAll
     static void Configuration() {
+        SelenideLogger.addListener("Allure", new AllureSelenide());
         TestsConfig config = ConfigFactory.create(TestsConfig.class, System.getProperties());
+        DesiredCapabilities capabilities = new DesiredCapabilities();
 
         String browserName = String.valueOf(config.browser());
         String browserVersion = config.version();
@@ -25,6 +32,10 @@ public class TestBase {
         Configuration.browserSize = browserResolution;
         Configuration.holdBrowserOpen = true;
 
+        Configuration.browserCapabilities = capabilities;
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
         if (config.remote()) {
             String selenoidLogin = config.selenoidLogin(),
                     selenoidPassword = config.selenoidPassword();
@@ -32,5 +43,17 @@ public class TestBase {
             Configuration.remote = String.format("https://%s:%s@selenoid.autotests.cloud/wd/hub",
                     selenoidLogin, selenoidPassword);
 
+        }
     }
-}}
+        @AfterEach
+        void addAttach() {
+            Attach.screenshotAs("Last screenshot");
+            Attach.pageSource();
+            Attach.browserConsoleLogs();
+            Attach.addVideo();
+        }
+
+
+    }
+
+
