09a1052e77f98171122d6b4c73f0d8fe8c7957bc
Kiriesha <49344266+Kiriesha@users.noreply.github.com>
2023-01-16 17:24:54 +0300

    changes

--- src/test/Java/config/TestsConfig.java
+++ src/test/Java/config/TestsConfig.java
@@ -2,20 +2,28 @@ package config;
 
 import com.codeborne.selenide.Configuration;
 import org.aeonbits.owner.Config;
-
+@Config.Sources({
+        "classpath:selenoid.properties"
+})
 public interface TestsConfig extends Config {
+    @Key("browser")
     @DefaultValue("chrome")
     String browser();
+    @Key("version")
     @DefaultValue("108.0")
     String version();
+    @Key("baseUrl")
     @DefaultValue("https://www.litres.ru")
     String baseUrl();
+    @Key("resolution")
     @DefaultValue("1920x1080")
     String resolution();
-    @DefaultValue("false")
-    boolean remote();
+//    @DefaultValue("false")
+    @Key("remote")
+    String remote();
 
-
+    @Key("selenoidLogin")
     String selenoidLogin();
+    @Key("selenoidPassword")
     String selenoidPassword();
 }
--- src/test/Java/tests/ui/TestBase.java
+++ src/test/Java/tests/ui/TestBase.java
@@ -3,57 +3,85 @@ package tests.ui;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import config.TestsConfig;
+import drivers.WebDriver;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.AutorisationPage;
 import pages.MainPage;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
     MainPage mainPage = new MainPage();
     AutorisationPage autoPage = new AutorisationPage();
 
     @BeforeAll
-    static void Configuration() {
-        SelenideLogger.addListener("Allure", new AllureSelenide());
-        TestsConfig config = ConfigFactory.create(TestsConfig.class, System.getProperties());
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        String browserName = String.valueOf(config.browser());
-        String browserVersion = config.version();
-        String browserResolution = config.resolution();
-
-        Configuration.browser = browserName;
-        Configuration.browserVersion = browserVersion;
-        Configuration.baseUrl = config.baseUrl();
-        Configuration.browserSize = browserResolution;
-        Configuration.holdBrowserOpen = true;
-
-        Configuration.browserCapabilities = capabilities;
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-
-        if (config.remote()) {
-            String selenoidLogin = config.selenoidLogin(),
-                    selenoidPassword = config.selenoidPassword();
-
-            Configuration.remote = String.format("https://%s:%s@selenoid.autotests.cloud/wd/hub",
-                    selenoidLogin, selenoidPassword);
-
-        }
+    static void setUp() {
+        WebDriver.configure();
     }
-        @AfterEach
-        void addAttach() {
-            Attach.screenshotAs("Last screenshot");
-            Attach.pageSource();
-            Attach.browserConsoleLogs();
-            Attach.addVideo();
-        }
 
-
+    @BeforeEach
+    public void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+    }
+}
+//    static void Configuration() {
+//        SelenideLogger.addListener("Allure", new AllureSelenide());
+//        TestsConfig config = ConfigFactory.create(TestsConfig.class, System.getProperties());
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//
+//        String browserName = String.valueOf(config.browser());
+//        String browserVersion = config.version();
+//        String browserResolution = config.resolution();
+
+//        Configuration.browser = browserName;
+//        Configuration.browserVersion = browserVersion;
+//        Configuration.baseUrl = config.baseUrl();
+//        Configuration.browserSize = browserResolution;
+//        Configuration.holdBrowserOpen = true;
+//
+//        Configuration.browserCapabilities = capabilities;
+//        capabilities.setCapability("enableVNC", true);
+//        capabilities.setCapability("enableVideo", true);
+//
+//        String configSource = config.remote();
+//
+//        if (configSource != null) {
+//            String selenoidLogin = config.selenoidLogin(),
+//                    selenoidPassword = config.selenoidPassword();
+//
+////        if (config.remote()) {
+////            String selenoidLogin = config.selenoidLogin(),
+////                    selenoidPassword = config.selenoidPassword();
+////
+//            Configuration.remote = String.format("https://%s:%s@selenoid.autotests.cloud/wd/hub",
+//                    selenoidLogin, selenoidPassword);
+//
+//        }
+//   }
+//        @AfterEach
+//        void addAttach() {
+//            Attach.screenshotAs("Last screenshot");
+//            Attach.pageSource();
+//            Attach.browserConsoleLogs();
+//            Attach.addVideo();
+//        }
+//
+//
+//    }
 
 
